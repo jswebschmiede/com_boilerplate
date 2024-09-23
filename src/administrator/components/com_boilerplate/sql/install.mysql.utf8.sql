5f0fbd8196aafcb63ed6295ba59789f95904c6d4
jswebschiede <schoeneburg.j@googlemail.com>
2024-09-23 21:57:25 +0200

    change: name to title and added created

--- src/administrator/components/com_boilerplate/sql/install.mysql.utf8.sql
+++ src/administrator/components/com_boilerplate/sql/install.mysql.utf8.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS `#__boilerplate_boilerplate` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`created_by` int(11) NOT NULL,
+	`created` datetime NOT NULL,
 	`ordering` int(11) NOT NULL,
 	`state` INT(11) NOT NULL DEFAULT 1,
-	`title` varchar(255) NOT NULL,
+	`title` varchar(255),
 	`description` text,
 	PRIMARY KEY (`id`)
 ) ENGINE = InnoDB COMMENT = '' DEFAULT COLLATE = utf8_general_ci;
