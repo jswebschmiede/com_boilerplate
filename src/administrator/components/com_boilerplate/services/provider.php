a6d0c834d8e918cd103d4cf4d1249812968ea2e7
jswebschiede <schoeneburg.j@googlemail.com>
2024-09-23 21:55:36 +0200

    add: admin compoent files, just boilerplate

---
+++ src/administrator/components/com_boilerplate/services/provider.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package     com_boilerplate
+ * @version     1.0.0
+ * @copyright   Copyright (C) 2024. All rights reserved.
+ * @license     MIT License (MIT) see LICENSE.txt
+ * @author      Jörg Schöneburg <info@joerg-schoeneburg.de> - https://joerg-schoeneburg.de
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\DI\Container;
+use Joomla\CMS\HTML\Registry;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\CMS\Extension\ComponentInterface;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\Component\Router\RouterFactoryInterface;
+use Joomla\CMS\Extension\Service\Provider\RouterFactory;
+// use Joomla\CMS\Extension\Service\Provider\CategoryFactory;
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
+use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
+use Joomla\Component\Boilerplate\Administrator\Extension\BoilerplateComponent;
+
+/**
+ * The mywalks service provider.
+ *
+ * @since  4.0.0
+ */
+return new class implements ServiceProviderInterface {
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function register(Container $container): void
+	{
+		// $container->registerServiceProvider(new CategoryFactory('\\Joomla\\Component\\Boilerplate'));
+		$container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\Boilerplate'));
+		$container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\Boilerplate'));
+		$container->registerServiceProvider(new RouterFactory('\\Joomla\\Component\\Boilerplate'));
+
+		$container->set(
+			ComponentInterface::class,
+			function (Container $container): BoilerplateComponent {
+				$component = new BoilerplateComponent($container->get(ComponentDispatcherFactoryInterface::class));
+
+				$component->setRegistry($container->get(Registry::class));
+				$component->setMVCFactory($container->get(MVCFactoryInterface::class));
+				// $component->setCategoryFactory($container->get(CategoryFactoryInterface::class)); 
+	
+				$component->setRouterFactory($container->get(RouterFactoryInterface::class));
+
+				return $component;
+			}
+		);
+	}
+};
---
+++ src/administrator/components/com_boilerplate/src/Extension/BoilerplateComponent.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @package     com_boilerplate
+ * @version     1.0.0
+ * @copyright   Copyright (C) 2024. All rights reserved.
+ * @license     MIT License (MIT) see LICENSE.txt
+ * @author      Jörg Schöneburg <info@joerg-schoeneburg.de> - https://joerg-schoeneburg.de
+ */
+
+namespace Joomla\Component\Boilerplate\Administrator\Extension;
+
+defined('JPATH_PLATFORM') or die;
+
+use Psr\Container\ContainerInterface;
+use Joomla\CMS\Extension\MVCComponent;
+use Joomla\CMS\HTML\HTMLRegistryAwareTrait;
+use Joomla\CMS\Component\Router\RouterServiceTrait;
+use Joomla\CMS\Extension\BootableExtensionInterface;
+use Joomla\CMS\Component\Router\RouterServiceInterface;
+
+/**
+ * Component class for com_boilerplate
+ *
+ * @since  4.0.0
+ */
+class BoilerplateComponent extends MVCComponent implements
+	BootableExtensionInterface,
+	RouterServiceInterface
+{
+	use RouterServiceTrait;
+	use HTMLRegistryAwareTrait;
+
+	/**
+	 * Booting the extension. This is the function to set up the environment of the extension like
+	 * registering new class loaders, etc.
+	 *
+	 * If required, some initial set up can be done from services of the container, eg.
+	 * registering HTML services.
+	 *
+	 * @param   ContainerInterface  $container  The container
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function boot(ContainerInterface $container): void
+	{
+
+	}
+}
